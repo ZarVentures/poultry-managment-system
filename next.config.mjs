40950636b97b43867d07f9f905a5f0fa969b78f1
cosmicdanish1 <cosmicdanish@outlook.com>
2026-03-03 14:24:26 +0530

    Add webpack alias configuration for @ path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
